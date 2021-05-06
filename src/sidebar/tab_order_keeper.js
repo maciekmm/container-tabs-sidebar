4d9b53fb8dd511256611ce3b84d53888ba181299
Maciej Mionskowski <git@maciekmm.net>
2021-05-06 21:46:16 +0200

    Make tab order keeper for with temporary containers, closes #93

--- src/sidebar/tab_order_keeper.js
+++ src/sidebar/tab_order_keeper.js
@@ -1,20 +1,50 @@
+import { isTemporaryContainer } from "./interop/temporary_containers.js"
+
 var tid
 function scheduleSortTabs() {
     if (!!tid) clearTimeout(tid)
-    tid = setTimeout(sortTabs, 100)
+    tid = setTimeout(sortTabs, 200)
 }
 
 async function getDesiredTabsOrder() {
     let tabs = await browser.tabs.query({ currentWindow: true, pinned: false })
-    let contextualIdentitites = (
-        await browser.contextualIdentities.query({})
-    ).map((ctx) => ctx.cookieStoreId)
+    let contextualIdentities = await Promise.all(
+        (await browser.contextualIdentities.query({})).map(
+            async (ctx, index) => {
+                const cookieStoreId = ctx.cookieStoreId
+                // temporary containers are treated visually as a single container but report different cookieStoreIds
+                // this will treat them as a single one
+                const desiredOrder = (await isTemporaryContainer(cookieStoreId))
+                    ? Number.MAX_SAFE_INTEGER
+                    : index + 1
+
+                return {
+                    cookieStoreId: cookieStoreId,
+                    order: desiredOrder,
+                }
+            }
+        )
+    )
+
+    let getDesiredContextualIdentityPosition = (cookieStoreId) => {
+        const containerInformation = contextualIdentities.find(
+            (ci) => ci.cookieStoreId === cookieStoreId
+        )
+        if (!containerInformation) {
+            return 0
+        }
+        return containerInformation.order
+    }
 
     let comparator = (tab1, tab2) => {
-        let tab1CI = contextualIdentitites.indexOf(tab1.cookieStoreId)
-        let tab2CI = contextualIdentitites.indexOf(tab2.cookieStoreId)
-        if (tab1CI !== tab2CI) {
-            return tab1CI - tab2CI
+        let tab1ContainerOrder = getDesiredContextualIdentityPosition(
+            tab1.cookieStoreId
+        )
+        let tab2ContainerOrder = getDesiredContextualIdentityPosition(
+            tab2.cookieStoreId
+        )
+        if (tab1ContainerOrder !== tab2ContainerOrder) {
+            return tab1ContainerOrder - tab2ContainerOrder
         }
         return tab1.index - tab2.index
     }
